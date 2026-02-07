70bc04195cdbdd9587c0cf7cfe6c6cb721cf78cf
Rahul <rahul433108k@gamil.com>
2026-02-07 20:33:59 +0530

    deployment fix

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from bot_data.truths import TRUTHS
+from bot_data.dares import DARES
+from bot_data.wyr import WOULD_YOU_RATHER
+from bot_data.roasts import ROASTS
+from bot_data.fates import FATES
+
+__all__ = [
+    "TRUTHS",
+    "DARES",
+    "WOULD_YOU_RATHER",
+    "ROASTS",
+    "FATES",
+]
